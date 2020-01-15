966c59f33a7043587b213f3d9571a00ede0ecb0c
Josphat <josphatkips@gmail.com>
2020-01-15 12:41:30 +0300

    bulk sms update

--- odoo-bulk-sms/models/TechnoHiveConfig.py
+++ odoo-bulk-sms/models/TechnoHiveConfig.py
@@ -1,5 +1,7 @@
 #-*- coding:utf-8 -*-
 from odoo import fields, models
+import urllib3
+import json
 
 class TechnoHiveConfig(models.Model):
     _name="technohive.config"
@@ -14,4 +16,16 @@ class TechnoHiveConfig(models.Model):
     ],required=True, default='draft')
 
     def button_send(self):
-        print("Josphat")
+        
+        http = urllib3.PoolManager()
+        URL = 'https://mysms.celcomafrica.com/api/services/sendsms/?apikey=" . urlencode($apikey) . "&partnerID=" . urlencode($partnerID) . "&message=" . urlencode($message) . "&shortcode=$shortcode&mobile=$mobile"'
+        root = '/notes/note'  # the path where the request handler is located
+        datas = http.request('GET', URL)
+        datas = json.loads(datas.data.decode('utf-8'))  # parses the response to  a compatible form
+        print(datas)
+        # for data in datas:
+        #     self.env['notes.note'].create({
+        #         'partner_id': self.env['res.partner.search'].search([('notes_user_id', '=', data['user_id'])]).id,
+        #         'note': data['note'],
+        #         'time_date': data['time']
+        #     })
--- odoo-bulk-sms/models/TechnoHiveContact.py
+++ odoo-bulk-sms/models/TechnoHiveContact.py
@@ -4,7 +4,7 @@ from odoo import models, fields
 
 class TechnoHiveContact(models.Model):
     _name="technohive.contact"
-    _inherit="mail.thread"
+    # _inherit="mail.thread"
     name=fields.Char(string="Name",required="True")
     phone_number=fields.Char('Phone Number', required="True",placeholder="Start with country code")
     description=fields.Text(string="Description", placeholder="Description about the customer")
