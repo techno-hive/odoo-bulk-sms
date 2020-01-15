7dd4e54f0a4eb456156d4741ff120f3f84ed7c2c
Josphat <josphatkips@gmail.com>
2020-01-15 16:07:16 +0300

    function to send the sms

--- odoo-bulk-sms/models/TechnoHiveSendSMS.py
+++ odoo-bulk-sms/models/TechnoHiveSendSMS.py
@@ -1,6 +1,8 @@
 #-*- coding:utf-8 -*-
 
 from odoo import models,fields
+import urllib3
+import json
 
 class TechnoHiveSendSMS(models.Model):
     _name='technohive.sendsms'
@@ -13,6 +15,17 @@ class TechnoHiveSendSMS(models.Model):
 
     def btn_sendsms(self):
         print(self)
+        apikey="josphat"
+        partnerID="josphat"
+        shortcode="shortcode"
+        message=self.text_message
         for f in self.phone_number:
+            http = urllib3.PoolManager()
+            URL = "https://mysms.celcomafrica.com/api/services/sendsms/?apikey=" + apikey + "&partnerID=" + partnerID \
+                  + "&message=" + message + "&shortcode="+ shortcode + "&mobile="+f.phone_number
+            root = '/notes/note'  # the path where the request handler is located
+            datas = http.request('GET', URL + root)
+            datas = json.loads(datas.data.decode('utf-8'))  # parses the response to  a compatible form
+            print(datas)
             print(f.phone_number)
             print(self.text_message)
