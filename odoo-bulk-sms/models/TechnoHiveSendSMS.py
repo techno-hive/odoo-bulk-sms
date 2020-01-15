de0257cbeeb023953a6080cbb76b92a1e7e9ec84
Josphat <josphatkips@gmail.com>
2020-01-15 17:09:03 +0300

    finished on send sms module

--- odoo-bulk-sms/models/TechnoHiveSendSMS.py
+++ odoo-bulk-sms/models/TechnoHiveSendSMS.py
@@ -14,18 +14,34 @@ class TechnoHiveSendSMS(models.Model):
     ],required="True", default='draft')
 
     def btn_sendsms(self):
-        print(self)
-        apikey="josphat"
-        partnerID="josphat"
-        shortcode="shortcode"
+        # print(self)
+        self.sendsms("+254713727937","Hey nigga")
+        val=self.env['technohive.config'].search(limit=1,[])
+        apikey=val.api_key
+        partnerID=val.partner_id
+        shortcode=val.sender_id
         message=self.text_message
         for f in self.phone_number:
             http = urllib3.PoolManager()
             URL = "https://mysms.celcomafrica.com/api/services/sendsms/?apikey=" + apikey + "&partnerID=" + partnerID \
                   + "&message=" + message + "&shortcode="+ shortcode + "&mobile="+f.phone_number
             root = '/notes/note'  # the path where the request handler is located
-            datas = http.request('GET', URL + root)
+            datas = http.request('GET', URL)
             datas = json.loads(datas.data.decode('utf-8'))  # parses the response to  a compatible form
             print(datas)
             print(f.phone_number)
             print(self.text_message)
+
+    def sendsms(self,phone, message):
+        val = self.env['technohive.config'].search(limit=1,[])
+
+
+        apikey = val.api_key
+        partnerID = val.partner_id
+        shortcode = val.sender_id
+        http = urllib3.PoolManager()
+        URL = "https://mysms.celcomafrica.com/api/services/sendsms/?apikey=" + apikey + "&partnerID=" + partnerID \
+              + "&message=" + message + "&shortcode=" + shortcode + "&mobile=" + phone
+        root = '/notes/note'  # the path where the request handler is located
+        datas = http.request('GET', URL)
+        datas = json
